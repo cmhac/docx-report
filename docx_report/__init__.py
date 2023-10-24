b45be1b932c7da3d1c21b12bbfda14b31eec3a59
Chris Hacker <49451910+christopher-hacker@users.noreply.github.com>
2023-10-24 15:10:02 +0000

    create __init__.py

---
+++ docx_report/__init__.py
@@ -0,0 +1,13 @@
+"""
+docx_report
+===========
+
+This module is a wrapper for the python-docx library to make it easier to create
+reports in Microsoft Word.
+
+Modules:
+    docx_report:
+        The main module for the docx_report package.
+"""
+
+from .docx_report import DocxReport
